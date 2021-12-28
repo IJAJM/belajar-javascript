8291d41422507b95dc43cf93042ad6a7d827cdf4
IJAJM <50859787+IJAJM@users.noreply.github.com>
2021-12-28 22:00:07 +0700

    operator penugasan javascript

---
+++ operator-penugasan.js
@@ -0,0 +1,12 @@
+var a = 1;
+a += 1;
+a *= 1;
+a /= 1;
+var a = 1;
+console.log('Nilai awal a :' + a);
+a += 1;
+console.log('Setelah ditambah 1 , Nilai a :' + a);
+a *= 3;
+console.log('Setelah dikali 3 , Nilai a :' + a);
+a /= 2;
+console.log('Setelah dibagi 2 , Nilai a :' + a);
