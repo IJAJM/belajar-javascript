7c3ab0aab54a32835e17d36fe02cf63b3d1bb4da
IJAJM <50859787+IJAJM@users.noreply.github.com>
2021-12-28 21:58:03 +0700

    array asosiatif pada javascript

---
+++ arrasosiatif.js
@@ -0,0 +1,15 @@
+//array berindeks
+var a = [10,20,30,40,50];
+//akses elemen menggunakan indeks 0. 1, 2, 3, ...
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+//array assosiatifvar b = {'jkt' : 'Jakarta', 'bdg' : 'Bandung', 'sby' : 'Surabaya',}
+var b = {'JKT' : 'Jakarta', 'BDG' : 'Bandung', 'SBY' : 'Surabaya', 'TSK' : 'Tasikmalaya'};
+//akses elemen menggunakan kunci
+console.log(b['TSK']);
+console.log(b['SBY']);
+console.log(b['JKT']);
+console.log(b['BDG']);
