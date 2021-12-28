f4fd34fd5fd4071b2899264f43fcb4c9b9bc7ae0
IJAJM <50859787+IJAJM@users.noreply.github.com>
2021-12-28 21:58:48 +0700

    pemrograman javascript pertama

---
+++ hello.js
@@ -0,0 +1,11 @@
+
+var s1 = `dengan adanya node.js, \n` +
+        `javascript dapat digunakan untuk membuat \n` +
+        `Beragam tipe aplikasi`;
+
+        var s2 = `dengan adanya Node.js,
+        javascript dapat digunakan untuk
+        membuat beragam tipe aplikasi` ;
+
+        console.log(` s1 ` + s1);
+        console.log(`\ns2 :` + s2);
