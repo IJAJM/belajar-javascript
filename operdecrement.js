4f07a49b2a58350880f470a4e8d41a9ae130e00c
IJAJM <50859787+IJAJM@users.noreply.github.com>
2021-12-28 22:00:57 +0700

    operator decrement javascript

---
+++ operdecrement.js
@@ -0,0 +1,14 @@
+var x;
+//pre-decrement
+console.log('pre-decrement');
+x = 9;
+console.log(`x \t: ${x}`);
+console.log(`--x \t: ${--x}`);
+console.log(`x \t: ${x}`);
+
+//post-decrement
+console.log('\nPost-decrement');
+x = 9;
+console.log(`x \t: ${x}`);
+console.log(`x-- \t: ${x--}`);
+console.log(`x \t: ${x}`);
