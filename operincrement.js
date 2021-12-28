0a20ab2d45da0b0e33e884f2817ebe7f4c23b251
IJAJM <50859787+IJAJM@users.noreply.github.com>
2021-12-28 22:01:31 +0700

    operator increment javascript

---
+++ operincrement.js
@@ -0,0 +1,14 @@
+var x;
+//pre-increment
+console.log('pre-increment');
+x = 9;
+console.log(`x \t : ${x}`);
+console.log(`++x \t : ${++x}`);
+console.log(`x \t : ${x}`);
+
+//post-increment
+console.log('\nPost-increment');
+x = 9;
+console.log(`x \t : ${x}`);
+console.log(`x++ \t : ${x++}`);
+console.log(`x \t : ${x}`);
