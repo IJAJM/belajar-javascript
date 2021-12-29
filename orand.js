87aa3ca64249a000708ede65ad2e161a4fcea632
IJAJM <50859787+IJAJM@users.noreply.github.com>
2021-12-30 04:24:59 +0700

    operator logika AND Javascript

---
+++ orand.js
@@ -0,0 +1,13 @@
+var a = true; b = true
+console.log('a && b');
+true && false
+false && false
+true && true
+false && true
+true || false
+false || true
+true || true
+false || false
+false || true
+true || true
+false || true
