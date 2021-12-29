3cc911cb1b5f7b5625fe4fd3dcccf68dd3657427
IJAJM <50859787+IJAJM@users.noreply.github.com>
2021-12-30 04:21:55 +0700

    operator bitwise pada javascript

---
+++ operator-bitwise.js
@@ -0,0 +1,8 @@
+var a = 12, b =10;
+
+console.log(`a \t: ${a}`);
+console.log(`b \t: ${b}`);
+console.log(`a & b \t: ${a & b}`);
+console.log(`a | b \t: ${a | b}`);
+console.log(`a ^ b \tIjajkeyboard ${a ^ b}`);
+console.log(`~a \t: ${~a}`);
